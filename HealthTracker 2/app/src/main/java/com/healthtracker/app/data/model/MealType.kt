d57ba7d316ed9f2685d1fa788f79c23b04b1b16a
darklotustab-wq <darklotustab@gmail.com>
2026-05-15 07:57:09 -0300

    Delete HealthTracker 2 directory

--- HealthTracker 2/app/src/main/java/com/healthtracker/app/data/model/MealType.kt
+++
@@ -1,8 +0,0 @@
-package com.healthtracker.app.data.model
-
-enum class MealType(val displayName: String) {
-    BREAKFAST("Desayuno"),
-    LUNCH("Almuerzo"),
-    SNACK("Merienda"),
-    DINNER("Cena")
-}
